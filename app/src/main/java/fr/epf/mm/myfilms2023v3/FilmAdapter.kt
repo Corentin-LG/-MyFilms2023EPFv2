c4514418c1f45005ccfcaa94309b83f52e820f58
Corentin-LG <73930639+Corentin-LG@users.noreply.github.com>
2023-06-08 17:31:09 +0200

    refresh auto

--- app/src/main/java/fr/epf/mm/myfilms2023v3/FilmAdapter.kt
+++ app/src/main/java/fr/epf/mm/myfilms2023v3/FilmAdapter.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import fr.epf.mm.myfilms2023v3.model.Film
@@ -19,6 +20,8 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class FilmViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+lateinit var appDatabase: FilmsDatabase
 
 class FilmAdapter(val context: Context, val films: List<Film>) :
 
@@ -49,7 +52,18 @@ class FilmAdapter(val context: Context, val films: List<Film>) :
 
         Glide.with(imageView).load(IMAGE_BASE + film.poster).into(imageView)
 
-        favouriteButton.setImageResource(R.drawable.baseline_favorite_border_24)
+        appDatabase = AppDatabase.getInstance(context)
+        var favFilmsToDisplay: Film?
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                favFilmsToDisplay = appDatabase.filmDao().findFilmsById(film.id)
+            }
+        }
+        if (favFilmsToDisplay == null) {
+            favouriteButton.setImageResource(R.drawable.baseline_favorite_border_24)
+        } else {
+            favouriteButton.setImageResource(R.drawable.baseline_favorite_24)
+        }
 
         cardView.click {
             val intent = Intent(context, DetailsFilmActivity::class.java)
@@ -58,8 +72,7 @@ class FilmAdapter(val context: Context, val films: List<Film>) :
         }
 
         favouriteButton.addFav {
-            var appDatabase: FilmsDatabase
-            appDatabase = AppDatabase.getInstance(context)
+            val appDatabase = AppDatabase.getInstance(context)
             val favFilmObject = films[position]
 
             var filmCherche: Film?
@@ -75,12 +88,14 @@ class FilmAdapter(val context: Context, val films: List<Film>) :
                         appDatabase.filmDao().insert(films[position])
                     }
                 }
+                favouriteButton.setImageResource(R.drawable.baseline_favorite_24)
             } else {
                 runBlocking {
                     withContext(Dispatchers.IO) {
                         appDatabase.filmDao().delete(films[position])
                     }
                 }
+                favouriteButton.setImageResource(R.drawable.baseline_favorite_border_24)
             }
         }
     }
